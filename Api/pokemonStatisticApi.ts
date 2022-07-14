24a5b04b44d155556c8b04aadf7642ac6c632882
KlaraKot <klara.tytus.kot@gmail.com>
2022-07-14 14:42:34 +0200

    dodanie informacji o abilities do widoku glownego

--- Api/pokemonStatisticApi.ts
+++ Api/pokemonStatisticApi.ts
@@ -4,21 +4,32 @@
 import type { Pokemon } from "../Types/pokemon";
 import { useEffect, useState } from "react";
 
-export const PokemonStatisticApi = (pokemon: Pokemon) => {
-  const [listOfStatistics, setListOfStatistics] = useState<Array<string>>([]);
+interface Stat {
+  ability: {
+    name: string;
+    url: string;
+  };
+}
 
+export const PokemonStatisticApi = (pokemon: Pokemon) => {
+  const [listOfStatistics, setListOfStatistics] = useState<Array<Stat>>([]);
   // sciagamy dane z api
   useEffect(() => {
     const getPokemons = async () => {
-      // await new Promise((res) => setTimeout(res, 1000));
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
       );
       const list = await response.json();
-      console.log(list.abilities);
+      setListOfStatistics(list.abilities);
     };
     getPokemons();
-  }, []);
+  }, [pokemon.name]);
+
+  // obrabianie danych
+
+  return {
+    maslana: listOfStatistics,
+  };
 };
 =======
 export const pokemonStatisticApi = () => {};
