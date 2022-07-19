07b6fab8f55dc2939362c9bf90d2051b4992b519
KlaraKot <klara.tytus.kot@gmail.com>
2022-07-19 13:23:42 +0200

    dodanie ikonek do tab navigator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { MyStack } from "./Navigator/MyStack";
 import { PokemonProvider } from "./Context/FavouritePokemonContext";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["ViewPropTypes will be removed from React Native."]);
 
 const App = () => (
   <PokemonProvider>
--- Screens/MainScreen.tsx
+++ Screens/MainScreen.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { ListOfPokemons } from "../Components/ListOfPokemons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ListOfFavouritePokemons } from "../Components/ListOfFavouritePokemons";
+import { Feather, AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
-export const MainScreen = () => (
-  <Tab.Navigator initialRouteName="ListOfPokemons">
-    <Tab.Screen name="ListOfPokemons" component={ListOfPokemons} />
-    <Tab.Screen
-      name="ListOfFavouritePokemons"
-      component={ListOfFavouritePokemons}
-    />
-  </Tab.Navigator>
-);
+export const MainScreen = () => {
+  const ListIcon = useMemo(() => <Feather name="list" size={40} />, []);
+  const HeartIcon = useMemo(() => <AntDesign name="heart" size={40} />, []);
+
+  return (
+    <Tab.Navigator initialRouteName="Lista Pokemonow">
+      <Tab.Screen
+        name="Lista Pokemonow"
+        component={ListOfPokemons}
+        options={{
+          tabBarLabel: "lista ulubionych",
+          tabBarIcon: () => ListIcon,
+        }}
+      />
+      <Tab.Screen
+        name="Lista ulubionych pokemonow"
+        component={ListOfFavouritePokemons}
+        options={{
+          tabBarLabel: "ulubione",
+          tabBarIcon: () => HeartIcon,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
